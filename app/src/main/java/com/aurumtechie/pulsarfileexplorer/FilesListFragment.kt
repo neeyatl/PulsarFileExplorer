b4dcedf1b85988fa8172dc7543b2c441703c527c
Neeyat Lotlikar <neeyat.lotlikar@gmail.com>
2020-05-21 16:15:16 +0530

    Initial commit

---
+++ app/src/main/java/com/aurumtechie/pulsarfileexplorer/FilesListFragment.kt
@@ -0,0 +1,122 @@
+package com.aurumtechie.pulsarfileexplorer
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.fragment.app.ListFragment
+import java.io.File
+
+class FilesListFragment : ListFragment() {
+
+    private var path: String = "/storage/emulated/0"
+
+    val currentPath
+        get() = path
+
+    companion object {
+        private const val PATH_EXTRA = "path_extra"
+
+        fun getInstance(path: String): FilesListFragment =
+            FilesListFragment().apply { this.path = path }
+
+        interface DirectoryExplorer {
+            fun onDirectoryClick(path: String)
+        }
+    }
+
+    private lateinit var directoryExplorer: DirectoryExplorer
+
+    private val values = mutableListOf<String>()
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        directoryExplorer = context as DirectoryExplorer
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (savedInstanceState != null) path = savedInstanceState.getString(PATH_EXTRA, path)
+
+        activity?.title = path
+
+        val dir = File(path)
+        if (!dir.canRead())
+            activity?.title = "${activity?.title} (inaccessible)"
+
+        val list = dir.list()
+        list?.let {
+            for (file in it)
+                if (!file.startsWith("."))
+                    values.add(file)
+        }
+        values.sort()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        listAdapter = context?.let {
+            ArrayAdapter(
+                it,
+                android.R.layout.simple_list_item_2,
+                android.R.id.text1,
+                values
+            )
+        }
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onListItemClick(l: ListView, v: View, position: Int, id: Long) {
+        super.onListItemClick(l, v, position, id)
+        var filename = listAdapter?.getItem(position)
+
+        filename = if (path.endsWith(File.separator)) path + filename
+        else path + File.separator + filename
+
+        if (File(filename).isDirectory)
+            directoryExplorer.onDirectoryClick(filename)
+        else startActivity(
+            Intent.createChooser(
+                Intent(Intent.ACTION_VIEW).apply {
+                    setDataAndType(Uri.fromFile(File(filename)), getMimeType(filename))
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                },
+                getString(R.string.open_using)
+            )
+        )
+    }
+
+    private fun getMimeType(url: String): String = when {
+        url.endsWith(".doc") || url.endsWith(".docx") -> "application/msword"
+        url.endsWith(".pdf") -> "application/pdf"
+        url.endsWith(".ppt") || url.endsWith(".pptx") -> "application/vnd.ms-powerpoint"
+        url.endsWith(".xls") || url.endsWith(".xlsx") -> "application/vnd.ms-excel"
+        url.endsWith(".zip") || url.endsWith(".rar") -> "application/x-wav"
+        url.endsWith(".rtf") -> "application/rtf"
+        url.endsWith(".wav") || url.endsWith(".mp3")
+                || url.endsWith(".m4a") -> "audio/x-wav"
+        url.endsWith(".gif") -> "image/gif"
+        url.endsWith(".jpg") || url.endsWith(".jpeg") || url.endsWith(".png") -> "image/jpeg"
+        url.endsWith(".txt") || url.endsWith(".mht")
+                || url.endsWith(".mhtml") || url.endsWith(".html") -> "text/plain"
+        url.endsWith(".3gp") || url.endsWith(".mpg")
+                || url.endsWith(".mpeg") || url.endsWith(".mpe")
+                || url.endsWith(".mp4") || url.endsWith(".avi") -> "video/*"
+        else -> "*/*"
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString(PATH_EXTRA, path)
+        super.onSaveInstanceState(outState)
+    }
+
+}
---
+++ app/src/main/java/com/aurumtechie/pulsarfileexplorer/MainActivity.kt
@@ -0,0 +1,112 @@
+package com.aurumtechie.pulsarfileexplorer
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+
+class MainActivity : AppCompatActivity(), FilesListFragment.Companion.DirectoryExplorer {
+
+    companion object {
+        private const val READ_REQUEST_CODE = 4579
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) initializeFileExplorer() // If permissions are given, update the UI.
+        else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            ) requestReadExternalStoragePermission()
+            else requestPermissionAndOpenSettings()
+        }
+    }
+
+    // Add the root directory fragment which is the first screen the user sees.
+    private fun initializeFileExplorer() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.directoryContainer, FilesListFragment())
+            .addToBackStack(null).commit()
+    }
+
+    // Invoked from the fragment. Adds another fragment to the back stack representing the new directory the user clicked on.
+    override fun onDirectoryClick(path: String) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.directoryContainer, FilesListFragment.getInstance(path))
+            .addToBackStack(null).commit()
+    }
+
+    override fun onBackPressed() {
+        // pop the root directory fragment and then exit the app
+        if (supportFragmentManager.backStackEntryCount == 1)
+            super.onBackPressed()
+        super.onBackPressed()
+
+        // Update the title as the path of the current fragment after popping the previous one
+        if (supportFragmentManager.backStackEntryCount > 0)
+            supportFragmentManager.findFragmentById(R.id.directoryContainer).let {
+                if (it is FilesListFragment)
+                    this@MainActivity.title = it.currentPath
+            }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            READ_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    initializeFileExplorer()
+                else if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        this,
+                        android.Manifest.permission.READ_EXTERNAL_STORAGE
+                    )
+                ) // If permission was denied once before but the user wasn't informed why the permission is necessary, do so.
+                    AlertDialog.Builder(this)
+                        .setMessage(R.string.read_external_storage_permission_rationale)
+                        .setPositiveButton(android.R.string.ok) { dialog, _ ->
+                            dialog.dismiss()
+                            requestReadExternalStoragePermission()
+                        }.show()
+                else /* If user has chosen to not be shown permission requests any longer,
+                     inform the user about it's importance and redirect her/him to device settings
+                     so that permissions can be given */
+                    requestPermissionAndOpenSettings()
+            }
+        }
+    }
+
+    private fun requestReadExternalStoragePermission() = ActivityCompat.requestPermissions(
+        this,
+        arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+        READ_REQUEST_CODE
+    )
+
+    private fun requestPermissionAndOpenSettings() = AlertDialog.Builder(this)
+        .setMessage(R.string.permission_request)
+        .setPositiveButton(R.string.show_settings) { dialog, _ ->
+            dialog.dismiss()
+            // Open application settings to enable the user to toggle the permission settings
+            startActivity(Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                data = Uri.fromParts("package", packageName, null)
+            })
+        }.show()
+
+}
